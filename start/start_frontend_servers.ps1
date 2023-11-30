be750577485d6865c9a687d9a0c5af25ac5560e7
南风过境 <109949105+sky1417168@users.noreply.github.com>
2023-11-30 16:17:49 +0800

    更新 start_frontend_servers.ps1

--- start/start_frontend_servers.ps1
+++ start/start_frontend_servers.ps1
@@ -1,5 +1,4 @@
 # 启动前端开发服务器
 Start-Process -NoNewWindow -FilePath "cmd.exe" -ArgumentList "/k cd frontend && npm run serve"
 
-# 启动后端 Django 服务器
-Start-Process -NoNewWindow -FilePath "cmd.exe" -ArgumentList "/k cd backend && python manage.py runserver"
+
